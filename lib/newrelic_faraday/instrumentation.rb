dd8f23041d9a9864ee1941a417314900ec70a77d
Kyle VanderBeek <kylev@kylev.com>
2014-06-11 18:00:08 -0700

    Modernize and simplify loading, logging, and disabling.
    Most importantly, the "named" method in NewRelic's bundled DependencyDetection
    automatically handles the "disable_faraday" flag and is the modern way to conditionally
    load the code here.
    
    There aren't any changes to how/what we instrument, it just all gets re-indented.

--- lib/newrelic_faraday/instrumentation.rb
+++ lib/newrelic_faraday/instrumentation.rb
@@ -1,26 +1,38 @@
-if defined?(::Faraday) and not NewRelic::Control.instance['disable_faraday'] and not ENV['NEWRELIC_ENABLE'].to_s =~ /false|off|no/i
-  Faraday::Connection.class_eval do
-    def run_request_with_newrelic_trace(method, url, params, headers, &block)
-      newrelic_host = parse_host_for_newrelic url
-      metrics = ["External/#{newrelic_host}/Faraday::Connection/#{method}", "External/#{newrelic_host}/all", "External/all"]
-      if NewRelic::Agent::Instrumentation::MetricFrame.recording_web_transaction?
-        metrics << "External/allWeb"
-      else
-        metrics << "External/allOther"
-      end
-      self.class.trace_execution_scoped metrics do
-        run_request_without_newrelic_trace(method, url, params, headers, &block)
-      end
-    end
-    alias run_request_without_newrelic_trace run_request
-    alias run_request run_request_with_newrelic_trace
+DependencyDetection.defer do
+  named :faraday
 
-    def parse_host_for_newrelic(url)
-      begin
-        return host if host
-        URI(url).host.to_s
-      rescue
-        ''
+  depends_on do
+    defined?(::Faraday)
+  end
+
+  executes do
+    ::NewRelic::Agent.logger.info 'Installing Faraday instrumentation'
+  end
+
+  executes do
+    Faraday::Connection.class_eval do
+      def run_request_with_newrelic_trace(method, url, params, headers, &block)
+        newrelic_host = parse_host_for_newrelic url
+        metrics = ["External/#{newrelic_host}/Faraday::Connection/#{method}", "External/#{newrelic_host}/all", "External/all"]
+        if NewRelic::Agent::Instrumentation::MetricFrame.recording_web_transaction?
+          metrics << "External/allWeb"
+        else
+          metrics << "External/allOther"
+        end
+        self.class.trace_execution_scoped metrics do
+          run_request_without_newrelic_trace(method, url, params, headers, &block)
+        end
+      end
+      alias run_request_without_newrelic_trace run_request
+      alias run_request run_request_with_newrelic_trace
+
+      def parse_host_for_newrelic(url)
+        begin
+          return host if host
+          URI(url).host.to_s
+        rescue
+          ''
+        end
       end
     end
   end
