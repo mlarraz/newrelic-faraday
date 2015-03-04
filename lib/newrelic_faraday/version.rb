c38b89bd157376a62f5ca9f0f8b50c0d69a46a59
Kyle VanderBeek <kylev@kylev.com>
2015-03-03 23:06:38 -0800

    Mark 0.7.0.
    The 0.7.0 series supports Faraday 0.9.x.

--- lib/newrelic_faraday/version.rb
+++ lib/newrelic_faraday/version.rb
@@ -1,8 +1,8 @@
 module NewrelicFaraday
   module VERSION
     MAJOR = '0'
-    MINOR = '6'
-    PATCH = '5'
+    MINOR = '7'
+    PATCH = '0'
     STRING = [MAJOR, MINOR, PATCH].join('.')
   end
 end
